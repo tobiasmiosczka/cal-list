e9c4133ccc344847e5fdf4c3e7b05bb1005f62a3
Tobias Miosczka <6351397+tobiasmiosczka@users.noreply.github.com>
2024-04-12 21:36:28 +0200

    fix: NO-TICKET added logging

--- src/main/java/com/github/tobiasmiosczka/callist/CalendarController.java
+++ src/main/java/com/github/tobiasmiosczka/callist/CalendarController.java
@@ -1,7 +1,10 @@
 package com.github.tobiasmiosczka.callist;
 
-import net.fortuna.ical4j.model.*;
+import net.fortuna.ical4j.model.Calendar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.UUID;
 
 @Controller
 public class CalendarController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalendarController.class);
+    private final static int SIZE = 365;
 
     private final SunEventService sunEventService;
 
@@ -21,27 +28,31 @@ public class CalendarController {
         this.sunEventService = sunEventService;
     }
 
-
-    @GetMapping("/sun/calendar.ics") //http://localhost:8080/sun/calendar.ics?latitude=51.56227&longitude=6.7434&altitude=0
+    //http://localhost:8080/sun/calendar.ics?latitude=51.56227&longitude=6.7434&altitude=0&token=1acd9867-bdda-4e56-afe1-32a7f7ad4913
+    @GetMapping("/sun/calendar.ics")
     @ResponseBody
     public byte[] getCalendars(
+            ServerHttpRequest serverHttpRequest,
+            @RequestParam UUID token,
             @RequestParam(required = false) Double latitude,
             @RequestParam(required = false) Double longitude,
             @RequestParam(required = false) Integer altitude) {
-        //final double latitude = 51.56227D;
-        //final double longitude = 6.7434D;
-        //final int altitude = 0;
+        LOGGER.info("Token: " + token
+                + " Remote: " + serverHttpRequest.getRemoteAddress()
+                + " Headers: " + serverHttpRequest.getHeaders().entrySet().stream()
+                .map(e -> e.getKey() + ": " + e.getValue())
+                .reduce((s1, s2) -> s1 + " " + s2)
+                .map(e -> "{" + e + "}")
+                .orElse("{}"));
         if (latitude == null || longitude == null || altitude == null) {
             return new byte[0];
         }
         final LocalDate now = LocalDate.now();
-        final int size = 365;
         final ZoneId zoneId = ZoneId.of("Europe/Berlin");
-        final Calendar calendar = sunEventService.getCalendar(latitude, longitude, altitude, now, size, zoneId);
+        final Calendar calendar = sunEventService.getCalendar(latitude, longitude, altitude, now, SIZE, zoneId);
         try {
             return sunEventService.convertCalendarToByteArray(calendar);
         } catch (IOException e) {
-
             return new byte[0];
         }
     }
